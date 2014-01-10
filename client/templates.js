5d75645ba36fd91958485f2bb2dbeda8d7b999e8
Julien Roubieu <julien@obiz.com.br>
2014-01-10 18:16:27 -0200

    Real buy and sell, with fixed 0.01 amount

--- client/templates.js
+++ client/templates.js
@@ -25,13 +25,13 @@ Template.buy.available = function () {
   var balance = Session.get("balance");
   return balance ? balance.usd_available : '-';
 };
-Template.buy.last = function () {
+Template.buy.best = function () {
   var ticker = Session.get("ticker");
-  return ticker ? ticker.last : '-';
+  return ticker ? ticker.ask : '-';
 };
 Template.buy.events({
-    'dblclick #buy-last': function () {
-      var price = $('#buy-last').text();
+    'dblclick #buy-best': function () {
+      var price = $('#buy-best').text();
       $('#buy-price').val(price);
     },
     'click #buy-clear': function () {
@@ -39,19 +39,54 @@ Template.buy.events({
     },
     'click #buy-dec': function () {
       var priceTxt = $('#buy-price').val().trim();
-      if (priceTxt == "") priceTxt = $('#buy-last').text();
+      if (priceTxt == "") priceTxt = $('#buy-best').text();
       $('#buy-price').val(parseFloat(priceTxt)-1);
     },
     'click #buy-inc': function () {
       var priceTxt = $('#buy-price').val().trim();
-      if (priceTxt == "") priceTxt = $('#buy-last').text();
+      if (priceTxt == "") priceTxt = $('#buy-best').text();
       $('#buy-price').val(parseFloat(priceTxt)+1);
     },
     'click #buy-button': function () {
       var price = $('#buy-price').val().trim();
-      if (price == "") price = $('#buy-last').text();
+      if (price == "") price = $('#buy-best').text();
       console.log('Buying at '+price);
-      //Meteor.call("buy_all", price);
+      Meteor.call("buy", 0.01,  price);
+    }
+});
+
+
+Template.sell.available = function () {
+  var balance = Session.get("balance");
+  return balance ? balance.btc_available : '-';
+};
+Template.sell.best = function () {
+  var ticker = Session.get("ticker");
+  return ticker ? ticker.bid : '-';
+};
+Template.sell.events({
+    'dblclick #sell-best': function () {
+      var price = $('#sell-best').text();
+      $('#sell-price').val(price);
+    },
+    'click #sell-clear': function () {
+      $('#sell-price').val('');
+    },
+    'click #sell-dec': function () {
+      var priceTxt = $('#sell-price').val().trim();
+      if (priceTxt == "") priceTxt = $('#sell-best').text();
+      $('#sell-price').val(parseFloat(priceTxt)-1);
+    },
+    'click #sell-inc': function () {
+      var priceTxt = $('#sell-price').val().trim();
+      if (priceTxt == "") priceTxt = $('#sell-best').text();
+      $('#sell-price').val(parseFloat(priceTxt)+1);
+    },
+    'click #sell-button': function () {
+      var price = $('#sell-price').val().trim();
+      if (price == "") price = $('#sell-best').text();
+      console.log('Selling at '+price);
+      Meteor.call("sell", 0.01,  price);
     }
 });
 
--- server/server.js
+++ server/server.js
@@ -1,73 +1,100 @@
-if (Meteor.isServer) {
+var Bitstamp = Meteor.require('bitstamp');
+var publicBitstamp = new Bitstamp();
+var conf = EJSON.parse(Assets.getText('bitstamp.config'));
+var privateBitstamp = new Bitstamp(conf.key, conf.secret, conf.client_id);
 
-  var Bitstamp = Meteor.require('bitstamp');
-  var publicBitstamp = new Bitstamp();
-  var conf = EJSON.parse(Assets.getText('bitstamp.config'));
-  var privateBitstamp = new Bitstamp(conf.key, conf.secret, conf.client_id);
+Meteor.startup(function () {
+  Meteor.setInterval(function() {
+    Meteor.call("ticker");
+  }, 30*1000);
+  Meteor.setInterval(function() {
+    Meteor.call("balance");
+  }, 59*1000);
+  Meteor.setInterval(function() {
+    Meteor.call("open_orders");
+  }, 27*1000);
+  Meteor.call("user_transactions");
+});
 
-  Meteor.startup(function () {
-    Meteor.setInterval(function() {
-      Meteor.call("ticker");
-    }, 30*1000);
-    Meteor.setInterval(function() {
-      Meteor.call("balance");
-    }, 59*1000);
-    Meteor.setInterval(function() {
-      Meteor.call("open_orders");
-    }, 27*1000);
-    Meteor.call("user_transactions");
-  });
-
-  Meteor.methods({
-    ticker: function() {
-      publicBitstamp.ticker(Meteor.bindEnvironment(function(err, ticker) {
-        if (err) throw err;
-        var lastTicker = Tickers.findOne({}, {sort: {timestamp: -1}});
-        if (lastTicker.timestamp != ticker.timestamp) {
-          Tickers.insert(ticker);
+Meteor.methods({
+  ticker: function() {
+    publicBitstamp.ticker(Meteor.bindEnvironment(function(err, ticker) {
+      if (err) throw err;
+      var lastTicker = Tickers.findOne({}, {sort: {timestamp: -1}});
+      if (lastTicker.timestamp != ticker.timestamp) {
+        Tickers.insert(ticker);
+      }
+    }));
+  },
+  balance: function() {
+    privateBitstamp.balance(Meteor.bindEnvironment(function(err, balance) {
+      if (err) throw err;
+      balance.timestamp = Math.floor(Date.now() / 1000);
+      Balances.insert(balance);
+    }));
+  },
+  buy: function(amount, price) {
+    if (amount * price < 1) {
+      console.log("Minimum order size is USD 1. Order not sent.");
+      return;
+    }
+    privateBitstamp.buy(amount, price, Meteor.bindEnvironment(function(err, order) {
+      if (err) throw err;
+      if (order.error) {
+        console.log(order.error.__all__);
+      }
+      else {
+        console.log("Buy order created for $" + (amount * price));
+        Orders.insert(order);
+        Meteor.call("balance");
+      }
+    }));
+  },
+  sell: function(amount, price) {
+    privateBitstamp.sell(amount, price, Meteor.bindEnvironment(function(err, order) {
+      if (err) throw err;
+      if (order.error) {
+        console.log(order.error.__all__);
+        return;
+      }
+      else {
+        console.log("Sell order created for $" + (amount * price));
+        Orders.insert(order);
+        Meteor.call("balance");
+      }
+    }));
+  },
+  open_orders: function() {
+    privateBitstamp.open_orders(Meteor.bindEnvironment(function(err, orders) {
+      if (err) throw err;
+      Orders.remove({});
+      _.each(orders, function(o){
+        Orders.insert(o);
+      });
+    }));
+  },
+  cancel_order: function(order_id) {
+    privateBitstamp.cancel_order(order_id, Meteor.bindEnvironment(function(err, success) {
+      if (err) throw err;
+      if (success) {
+        console.log("Order was cancelled");
+        Orders.remove({"id":order_id});
+        Meteor.call("balance");
+      }
+      else {
+        console.log("Bistamp did NOT cancel the order");
+      }
+    }));
+  },
+  user_transactions: function() {
+    privateBitstamp.user_transactions(100, Meteor.bindEnvironment(function(err, transactions) {
+      if (err) throw err;
+      _.each(transactions, function(t){
+        var count = Transactions.find({'id': t.id}).count();
+        if (count === 0) {
+          Transactions.insert(t);
         }
-      }));
-    },
-    balance: function() {
-      privateBitstamp.balance(Meteor.bindEnvironment(function(err, balance) {
-        if (err) throw err;
-        balance.timestamp = Math.floor(Date.now() / 1000);
-        Balances.insert(balance);
-      }));
-    },
-    open_orders: function() {
-      privateBitstamp.open_orders(Meteor.bindEnvironment(function(err, orders) {
-        if (err) throw err;
-        Orders.remove({});
-        _.each(orders, function(o){
-          Orders.insert(o);
-        });
-      }));
-    },
-    cancel_order: function(order_id) {
-      privateBitstamp.cancel_order(order_id, Meteor.bindEnvironment(function(err, success) {
-        if (err) throw err;
-        if (success) {
-          console.log("Order was cancelled");
-          Orders.remove({"id":order_id});
-          Meteor.call("balance");
-        }
-        else {
-          console.log("Bistamp did NOT cancel the order");
-        }
-      }));
-    },
-    user_transactions: function() {
-      privateBitstamp.user_transactions(100, Meteor.bindEnvironment(function(err, transactions) {
-        if (err) throw err;
-        _.each(transactions, function(t){
-          var count = Transactions.find({'id': t.id}).count();
-          if (count === 0) {
-            Transactions.insert(t);
-          }
-        });
-      }));
-    },
-  });
-
-}
+      });
+    }));
+  },
+});
