e81d64cde521d4dc43b925a80777a53760722477
Julien Roubieu <julien@obiz.com.br>
2014-01-28 22:58:35 -0200

    Added SmartCollections https://github.com/arunoda/meteor-smart-collections

--- bitstampc.js
+++ bitstampc.js
@@ -1,9 +1,9 @@
 
 // Model collections. See Bitstamp API doc for schema
-Tickers = new Meteor.Collection("tickers");
-Balances = new Meteor.Collection("balances");
-Transactions = new Meteor.Collection("transactions");
-Orders = new Meteor.Collection("orders");
+Tickers = new Meteor.SmartCollection("tickers");
+Balances = new Meteor.SmartCollection("balances");
+Transactions = new Meteor.SmartCollection("transactions");
+Orders = new Meteor.SmartCollection("orders");
 
 if (Meteor.isClient) {
 	Session.setDefault("balance", {
